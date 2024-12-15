8aafc28d99986403183fc995ab19f3bd1966a17a
Foobar <github@foobar.com>
2024-12-15 22:48:48 +0800

    ...

--- data.js
+++ data.js
@@ -2,32 +2,32 @@ const data = {
   动画: [
     {
       name: "動漫花園資源網",
-      url: "https://go.bkmk.link/5ndwhj",
+      url: "https://share.dmhy.org",
       icon: "/icons/share.dmhy.org.svg",
     },
     {
       name: "柯南事务所",
-      url: "https://go.bkmk.link/thn32t",
+      url: "https://share.dmhy.org/topics/list/team_id/75",
       icon: "/icons/share.dmhy.org.svg",
     },
     {
       name: "星光字幕组",
-      url: "https://go.bkmk.link/hnb8rq",
+      url: "https://share.dmhy.org/topics/list/team_id/299",
       icon: "/icons/share.dmhy.org.svg",
     },
     {
       name: "银色子弹字幕组",
-      url: "https://go.bkmk.link/tmc2pv",
+      url: "https://share.dmhy.org/topics/list/team_id/576",
       icon: "/icons/share.dmhy.org.svg",
     },
     {
       name: "名侦探柯南动画集数列表",
-      url: "https://go.bkmk.link/gsyrc4",
+      url: "https://zh.wikipedia.org/wiki/名偵探柯南動畫集數列表",
       iconify: "mdi:wikipedia",
     },
     {
       name: "银色子弹数据站",
-      url: "https://go.bkmk.link/ua66rp",
+      url: "https://sbsub.com/data",
       icon: "/icons/sbsub.com.svg",
       size: "48px",
     },
@@ -35,52 +35,52 @@ const data = {
   工具: [
     {
       name: "ASCII Tree Generator",
-      url: "https://go.bkmk.link/ptvj95",
+      url: "https://tree.nathanfriend.io",
       iconify: "mdi:file-tree",
     },
     {
       name: "Cloud Convert",
-      url: "https://go.bkmk.link/5uwt97",
+      url: "https://cloudconvert.com",
       iconify: "ant-design:cloud-sync-outlined",
     },
     {
       name: "CSS Mesh Gradients Generator",
-      url: "https://go.bkmk.link/e74ywj",
+      url: "https://csshero.org/mesher",
       icon: "/icons/csshero.org_mesher.svg",
       size: "32px",
     },
     {
       name: "Favicon Generator",
-      url: "https://go.bkmk.link/huumfg",
+      url: "https://favicon.io",
       iconText: "F",
       fontFamily: "Leckerli One, system-ui",
     },
     {
       name: "Google 翻译",
-      url: "https://go.bkmk.link/mjppm7",
+      url: "https://translate.google.com",
       iconify: "mdi:translate",
       size: "32px",
     },
     {
       name: "JSON Editor Online",
-      url: "https://go.bkmk.link/wrk7ph",
+      url: "https://jsoneditoronline.org",
       iconify: "lets-icons:json",
     },
     {
       name: "Time.is",
-      url: "https://go.bkmk.link/dh4ebr",
+      url: "https://time.is",
       iconify: "mdi:clock-time-three",
     },
     {
       name: "TinyPNG",
-      url: "https://go.bkmk.link/exrh79",
+      url: "https://tinypng.com",
       icon: "/icons/tinypng.com.png",
       size: "100%",
       radius: "16px",
     },
     {
       name: "TOTP Token Generator",
-      url: "https://go.bkmk.link/ta6whu",
+      url: "https://totp-token-generator.pages.dev",
       icon: "/icons/totp-token-generator.pages.dev.svg",
       size: "32px",
     },
@@ -88,19 +88,19 @@ const data = {
   脚本: [
     {
       name: "ECS",
-      url: "https://go.bkmk.link/ccrn23",
+      url: "https://github.com/spiritLHLS/ecs",
       iconify: "mdi:github",
     },
     {
       name: "Oracle Server Keep Alive Script",
-      url: "https://go.bkmk.link/jyfywp",
+      url: "https://github.com/spiritLHLS/Oracle-server-keep-alive-script",
       iconify: "mdi:github",
     },
   ],
   人工智能: [
     {
       name: "ChatGPT",
-      url: "https://go.bkmk.link/u35swd",
+      url: "https://chatgpt.com",
       iconify: "simple-icons:openai",
       size: "32px",
     },
@@ -108,13 +108,13 @@ const data = {
   设计: [
     {
       name: "Iconify",
-      url: "https://go.bkmk.link/uggjn2",
+      url: "https://icon-sets.iconify.design",
       iconify: "simple-icons:iconify",
       size: "32px",
     },
     {
       name: "Photopea",
-      url: "https://go.bkmk.link/bkqczu",
+      url: "https://photopea.com",
       iconify: "simple-icons:photopea",
       size: "32px",
     },
@@ -122,32 +122,32 @@ const data = {
   域名: [
     {
       name: "比较所有顶级域名的价格",
-      url: "https://go.bkmk.link/k7qfe3",
+      url: "https://zh-hans.tld-list.com",
       iconText: "TLD\nLIST",
       size: "16px",
       fontWeight: 600,
     },
     {
       name: "Cloudflare",
-      url: "https://go.bkmk.link/hcr5yj",
+      url: "https://cloudflare.com",
       iconify: "simple-icons:cloudflare",
     },
     {
       name: "Spaceship",
-      url: "https://go.bkmk.link/kgv7r2",
+      url: "https://spaceship.com",
       iconify: "simple-icons:spaceship",
       size: "32px",
     },
     {
       name: "WHO.CX",
-      url: "https://go.bkmk.link/g56bmk",
+      url: "https://who.cx",
       icon: "/icons/who.cx.svg",
     },
   ],
   资料: [
     {
       name: "维基百科",
-      url: "https://go.bkmk.link/ygxpqb",
+      url: "https://zh.wikipedia.org/wiki/Wikipedia:首页",
       iconify: "mdi:wikipedia",
     },
   ],
